41c070a562b404c3f39e469326a73aacf0a80ccc
Sejal Godbole <sejalgodbole02@gmail.com>
2024-12-08 03:47:10 +0000

    rename the folder

--- 01_ColorChanger/script.js
+++ 01_ColorChanger/script.js
@@ -2,6 +2,7 @@ const buttons = document.querySelectorAll('.button')
 const body = document.querySelector('body')
 
 buttons.forEach(function (button){
+    
     console.log(button)
     button.addEventListener('click', function(e) {
         console.log(e)
