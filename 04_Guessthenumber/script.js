9ae0fc417e157d60f13b2f9e16f6495b994cb06b
Sejal Godbole <sejalgodbole02@gmail.com>
2024-12-09 18:03:09 +0000

    done

--- 04_Guessthenumber/script.js
+++ 04_Guessthenumber/script.js
@@ -1,4 +1,4 @@
-const randomNumber = parseInt(Math.random() * 100 + 1);
+let randomNumber = parseInt(Math.random() * 100 + 1);
 
 const submit = document.querySelector('#subt');
 const userInput = document.querySelector('#guessField');
@@ -31,7 +31,7 @@ function validateGuess (guess) {
         alert('Please enter a valid number less than 100')
     } else {
         prevGuess.push(guess)
-        if (numGuess === 11) {
+        if (numGuess === 10) {
             displayGuess(guess)
             displayMessage(`Game Over, Random number was ${randomNumber}`)
             endGame()
@@ -65,9 +65,26 @@ function displayMessage (message) {
 }
 
 function endGame () {
-
+    userInput.value = ''
+    userInput.setAttribute('disabled', '')
+    p.classList.add('button')
+    p.innerHTML = `<h2 id = 'newGame'>Start new Game</h2>`;
+    startOver.appendChild(p)
+    playGame = false
+    newGame()
 }
 
 function newGame () {
-
+    const newGameButton = document.querySelector('#newGame')
+    newGameButton.addEventListener('click', function(e){
+        let randomNumber = parseInt(Math.random() * 100 + 1);
+        prevGuess = []
+        numGuess = 1
+        guessSlot.innerHTML = ''
+        remaining.innerHTML = `${11 - numGuess}`
+        lowOrHi.innerHTML = ''
+        userInput.removeAttribute('disabled')
+        startOver.removeChild(p)
+        playGame = true
+    })
 }
